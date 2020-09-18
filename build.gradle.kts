11872b2804c998ceafbc3f3ebc62c7ccbb56fda2
wybosys <wybosys@gmail.com>
2020-09-18 15:16:04 +0800

    mirror

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,14 @@ group = "com.nnt"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    maven { setUrl("https://maven.aliyun.com/repository/jcenter") }
     maven { setUrl("https://maven.aliyun.com/repository/central") }
     maven { setUrl("https://maven.aliyun.com/repository/public") }
+    maven { setUrl("https://maven.aliyun.com/repository/google") }
+    maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
+    maven { setUrl("https://maven.aliyun.com/repository/snapshots") }
+    maven { setUrl("https://maven.aliyun.com/repository/spring") }
+    maven { setUrl("https://maven.aliyun.com/repository/spring-plugin") }
 }
 
 dependencies {
