9a10e343deec94d1bbe0b1d8e1474013f10f595d
wybosys <wybosys@gmail.com>
2020-10-07 14:14:28 +0800

    prj

--- src/main/com/nnt/store/rdb.kt
+++ src/main/com/nnt/store/rdb.kt
@@ -1,5 +1,7 @@
 package com.nnt.store
 
+typealias AffectedRows = Int
+
 abstract class AbstractRdb : AbstractDbms() {
 
 }
--- src/main/com/test/dao.kt
+++ src/main/com/test/dao.kt
@@ -1,12 +1,16 @@
 package com.test
 
+import com.nnt.store.AffectedRows
+
+// dao入口，通过 resources/mybatis/sample.xml 绑定，并通过 app.json 进行注册
 interface Sample {
     fun listEchoo(): List<Echoo>
     fun echoo(m: Echoo)
-    fun clearEchoo(): Int
-    fun updateEchoo(m: Echoo): Int
+    fun clearEchoo(): AffectedRows
+    fun updateEchoo(m: Echoo): AffectedRows
 }
 
+// dao使用的模型类
 class Echoo {
 
     var id: Int = 0
