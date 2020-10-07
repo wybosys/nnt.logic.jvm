77680e190f9e4f74cd74bf96ca25c5db06572d60
wybosys <wybosys@gmail.com>
2020-10-07 11:51:29 +0800

    test

--- test/test.py
+++ test/test.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
 import grpc
-import test_pb2
-import test_pb2_grpc
+from test_pb2_grpc import TestStub
 from google.protobuf.empty_pb2 import Empty
 
 # 当前启动的测试服务
@@ -12,6 +11,6 @@ host = "localhost:8092"
 channel = grpc.insecure_channel(host)
 
 # 构造数据
-stub = test_pb2_grpc.TestStub(channel)
+stub = TestStub(channel)
 response = stub.hello(Empty())
 print("收到数据 %s" % response.message)
