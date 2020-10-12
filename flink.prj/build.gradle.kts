cd12a107b892d9eb5268400a3461fc8f95b8ac27
wybosys <wybosys@gmail.com>
2020-10-12 11:35:28 +0800

    test

--- flink.prj/build.gradle.kts
+++ flink.prj/build.gradle.kts
@@ -1,7 +1,12 @@
 plugins {
     java
+    idea
     kotlin("jvm") version "1.4.10"
     kotlin("kapt") version "1.4.10"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
 }
 
 group = "com.nnt"
@@ -51,35 +56,37 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9-native-mt")
 
     // logic
-    implementation("com.google.auto.service:auto-service:1.0-rc7")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.9.9.3")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.10.0")
     implementation("com.ctrip.framework.apollo:apollo-client:1.7.0")
 
     // db
     implementation("org.mybatis:mybatis:3.5.5")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.3.4.RELEASE")
+    implementation("com.alibaba:druid:1.1.24")
     implementation("mysql:mysql-connector-java:8.0.21")
     implementation("redis.clients:jedis:3.3.0")
-    implementation("org.apache.hbase:hbase:2.3.2")
-    implementation("org.neo4j:neo4j:4.1.2")
-    implementation("org.apache.phoenix:phoenix:4.15.0-HBase-1.4")
+    implementation("org.neo4j.driver:neo4j-java-driver:4.1.1")
+    implementation("org.apache.hbase:hbase-client:2.2.6")
+    implementation("org.apache.phoenix:phoenix-queryserver-client:5.0.0-HBase-2.0")
+    implementation("org.elasticsearch.client:elasticsearch-rest-high-level-client:7.9.2")
 
     // grpc
     implementation("com.google.protobuf:protobuf-java:3.12.0")
     implementation("com.google.protobuf:protobuf-java-util:3.12.0")
     implementation("io.grpc:grpc-all:1.32.1")
 
+    // dubbo-spring
+    implementation("org.springframework:spring-jdbc:4.3.8.RELEASE")
+
     // flink
-    implementation("com.alibaba.blink:flink-java:blink-3.7.0")
-    implementation("com.alibaba.blink:flink-streaming-java_2.11:blink-3.7.0")
-    implementation("com.alibaba.blink:flink-clients_2.11:blink-3.7.0")
-    implementation("org.apache.flink:statefun-sdk:2.1.0")
-    implementation("org.apache.flink:statefun-flink-harness:2.1.0")
+    implementation("org.apache.flink:flink-java:1.11.2")
+    implementation("org.apache.flink:flink-streaming-java_2.11:1.11.2")
+    implementation("org.apache.flink:flink-clients_2.11:1.11.2")
     implementation("org.apache.bahir:flink-connector-redis_2.11:1.0")
-    implementation("com.alibaba.blink:flink-connector-kafka-0.10_2.11:blink-3.7.0")
-    implementation("com.alibaba.ververica:flink-connector-mysql-cdc:1.1.0")
+    implementation("org.apache.flink:flink-connector-kafka_2.11:1.11.2")
+    implementation("org.apache.flink:flink-connector-filesystem_2.11:1.11.2")
+    implementation("org.apache.flink:flink-connector-jdbc_2.11:1.11.2")
     implementation("org.apache.flink:flink-connector-hbase_2.11:1.11.2")
-    implementation("com.alibaba.blink:flink-connector-elasticsearch-base_2.11:blink-3.7.0")
+    implementation("org.apache.flink:flink-connector-elasticsearch-base_2.11:1.11.2")
 
     // test
     testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
