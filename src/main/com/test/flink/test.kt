3b121806b51f6615686b30f93df7bd0b9c9d122f
wybosys <wybosys@gmail.com>
2020-10-22 16:19:24 +0800

    clean

--- src/main/com/test/flink/test.kt
+++ src/main/com/test/flink/test.kt
@@ -1,24 +1,15 @@
 package com.test.flink
 
 import com.nnt.flink.Task
-import com.nnt.manager.Servers
-import com.nnt.server.Kafka
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
 
 class Test : Task() {
 
     override fun start() {
         super.start()
 
-        //val env = StreamExecutionEnvironment.getExecutionEnvironment()
+        val env = StreamExecutionEnvironment.getExecutionEnvironment()
 
-        //env.execute(id)
-
-        val k = Servers.Find("kafka") as Kafka
-        val cli = k.instanceClient()
-        cli.open("xaas-logs")
-        cli.subscribe { msg, _ ->
-            println(msg)
-        }
-        cli.produce("hello")
+        env.execute(id)
     }
 }
