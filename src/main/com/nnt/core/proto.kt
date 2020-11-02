f9508f9e1cbd4e7284b726eaaba7169456066573
wybosys <wybosys@gmail.com>
2020-11-02 21:07:04 +0800

    prj

---
+++ src/main/com/nnt/core/proto.kt
@@ -0,0 +1,33 @@
+package com.nnt.core
+
+// model的参数
+
+// 隐藏该model
+const val hidden = "hidden";
+
+// 需要登录
+const val auth = "auth";
+
+// 是一个枚举
+const val enumm = "enumm";
+
+// 定义一组const对象
+const val constant = "constant";
+
+// field的参数
+// 可选的参数
+const val optional = "optional";
+
+// 必须的参数，不提供则忽略
+const val required = "required";
+
+// 输入输出
+const val input = "input";
+const val output = "output";
+
+// 为了给array定义数据，不能直接用系统类型
+const val string_t = "string";
+const val integer_t = "integer";
+const val double_t = "double";
+const val number_t = "number";
+const val boolean_t = "boolean";
