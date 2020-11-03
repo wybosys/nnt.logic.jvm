73d7ca89c3230cf5e99c0b983feb026c8f3044a9
wybosys <wybosys@gmail.com>
2020-11-03 17:16:17 +0800

    prj

--- src/main/com/test/app/router/sample.kt
+++ src/main/com/test/app/router/sample.kt
@@ -1,14 +1,22 @@
 package com.test.app.router
 
+import com.nnt.core.IRouter
+import com.nnt.core.Jsonobj
 import com.nnt.core.action
 import com.test.app.model.Echoo
 import com.test.app.model.Trans
 
-class RTest {
+class RSample : IRouter {
+
+    override val action = "test"
 
     @action(Echoo::class)
     fun echo(trans: Trans) {
 
+    }
+
+    override fun config(node: Jsonobj) {
+        // pass
     }
 
 }
