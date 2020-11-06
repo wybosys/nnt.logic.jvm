3f3c022185fe7b27b128d8957889d10d430e321e
wybosys <wybosys@gmail.com>
2020-11-06 17:40:14 +0800

    dubbo编译

--- src/main/com/nnt/dubbo/dubbo.kt
+++ src/main/com/nnt/dubbo/dubbo.kt
@@ -1,6 +1,6 @@
 package com.nnt.dubbo
 
-import com.nnt.core.Jsonobj
+import com.nnt.core.JsonObject
 import com.nnt.core.logger
 import com.nnt.manager.App
 import com.nnt.server.AbstractServer
@@ -34,12 +34,12 @@ open class Dubbo : AbstractServer() {
     private val _protocols = mutableMapOf<String, DubboProtocol>()
     private val _services = mutableMapOf<String, DubboService>()
 
-    override fun config(cfg: Jsonobj): Boolean {
+    override fun config(cfg: JsonObject): Boolean {
         if (!super.config(cfg))
             return false
 
         // 设置应用配置
-        _name = (cfg["name"] ?: cfg["id"]).asText()
+        _name = (cfg["name"] ?: cfg["id"])!!.asString()
 
         // 获取注册中心配置
         if (!cfg.has("registry")) {
@@ -47,18 +47,18 @@ open class Dubbo : AbstractServer() {
             return false
         }
 
-        val cfg_reg = cfg["registry"]
-        if (cfg_reg["type"] == null) {
+        val cfg_reg = cfg["registry"]!!
+        if (!cfg_reg.has("type")) {
             logger.fatal("registry丢失type配置")
             return false
         }
 
-        val cfg_reg_type = cfg_reg["type"].asText()
+        val cfg_reg_type = cfg_reg["type"]!!.asString()
         when (cfg_reg_type) {
             "zookeeper" -> {
                 _registry = DubboRegistryCfg()
                 _registry.type = cfg_reg_type
-                val host = cfg_reg["host"].asText()
+                val host = cfg_reg["host"]!!.asString()
                 _registry.host = "zookeeper://${host}"
             }
             else -> {
@@ -74,11 +74,11 @@ open class Dubbo : AbstractServer() {
             logger.fatal("没有配置service")
             return false
         }
-        for (e in cfg_service) {
+        for (e in cfg_service.asArray()) {
             val svc = DubboService()
-            svc.id = e["id"].asText()
+            svc.id = e["id"]!!.asString()
 
-            var clsnm = e["impl"].asText()
+            var clsnm = e["impl"]!!.asString()
             var cls = App.shared.findEntry(clsnm)
             if (cls == null) {
                 logger.fatal("没有找到类型 ${clsnm}")
@@ -86,7 +86,7 @@ open class Dubbo : AbstractServer() {
             }
             svc.impl = clsnm
 
-            clsnm = e["iface"].asText()
+            clsnm = e["iface"]!!.asString()
             cls = App.shared.findEntry(clsnm)
             if (cls == null) {
                 logger.fatal("没有找到类型 ${clsnm}")
@@ -104,19 +104,19 @@ open class Dubbo : AbstractServer() {
             logger.fatal("没有配置protocol")
             return false
         }
-        for (e in cfg_protocol) {
+        for (e in cfg_protocol.asArray()) {
             val pt = DubboProtocol()
 
-            pt.type = e["type"].asText()
+            pt.type = e["type"]!!.asString()
             when (pt.type) {
                 "dubbo" -> {
-                    pt.port = e["port"].asInt()
+                    pt.port = e["port"]!!.asInteger().toInt()
                 }
                 "rest" -> {
-                    pt.port = e["port"].asInt()
+                    pt.port = e["port"]!!.asInteger().toInt()
                 }
                 "grpc" -> {
-                    pt.port = e["port"].asInt()
+                    pt.port = e["port"]!!.asInteger().toInt()
                 }
                 else -> {
                     logger.fatal("不支持该协议 ${pt.type}")
@@ -129,8 +129,8 @@ open class Dubbo : AbstractServer() {
                 logger.fatal("service数据错误")
                 return false
             }
-            for (esvc in svc) {
-                val svcid = esvc.asText()
+            for (esvc in svc.asArray()) {
+                val svcid = esvc.asString()
                 val fnd = _services[svcid]
                 if (fnd == null) {
                     logger.fatal("没有找到服务 ${svcid}")
