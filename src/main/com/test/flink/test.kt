cfbdaf26d2e58efd8200e21d040ac4a676162b95
wybosys <wybosys@gmail.com>
2020-11-07 13:37:07 +0800

    addtest

--- src/main/com/test/flink/test.kt
+++ src/main/com/test/flink/test.kt
@@ -9,6 +9,10 @@ class Test : Task() {
         super.start()
 
         val env = StreamExecutionEnvironment.getExecutionEnvironment()
+        env.parallelism = 1
+
+        val ds = env.fromCollection(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9))
+        ds.print()
 
         env.execute(id)
     }
