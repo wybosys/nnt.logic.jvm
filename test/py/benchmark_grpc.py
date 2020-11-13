edf7c323806cc099d530c95f09206d2cb4f429bb
wybosys <wybosys@gmail.com>
2020-11-13 17:25:30 +0800

    prj

---
+++ test/py/benchmark_grpc.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import time
+from dubbo.test_pb2_grpc import TestStub
+from google.protobuf.empty_pb2 import Empty
+from google.protobuf.wrappers_pb2 import StringValue
+
+import grpc
+
+# 当前启动的测试服务
+host = "localhost:8093"
+
+# 链接服务器
+channel = grpc.insecure_channel(host)
+stub = TestStub(channel)
+
+
+def test():
+    beg = time.time_ns()
+
+    # 测试hello
+    response = stub.hello(Empty())
+    # print("收到数据 %s" % response.message)
+
+    end = time.time_ns()
+    cost = end - beg
+    print("消耗 %f ms" % (cost / 1000000))
+
+
+if __name__ == "__main__":
+    while (1):
+        for i in range(0, 50):
+            test()
+        time.sleep(1)
