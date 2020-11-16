39a71eb4d4fbb223c4f10417aaac0a21dd784d84
wybosys <wybosys@gmail.com>
2020-11-16 22:15:29 +0800

    opt

--- app.prj/build.gradle.kts
+++ app.prj/build.gradle.kts
@@ -70,12 +70,12 @@ sourceSets {
     }
 }
 
-fun <T : Any> OsUse(win: T, unix: T, mac: T): T {
+fun <T : Any> OsUse(win: () -> T, unix: () -> T, mac: () -> T): T {
     if (Os.isFamily(Os.FAMILY_WINDOWS))
-        return win
+        return win()
     if (Os.isFamily(Os.FAMILY_MAC))
-        return mac
-    return unix
+        return mac()
+    return unix()
 }
 
 dependencies {
@@ -90,8 +90,8 @@ dependencies {
     implementation("com.ctrip.framework.apollo:apollo-client:1.7.0")
     implementation("it.sauronsoftware.cron4j:cron4j:2.2.5")
     implementation("joda-time:joda-time:2.10.8")
-    implementation("com.eclipsesource.j2v8:j2v8_${OsUse("windows", "linux", "mac")}_x86_64:4.8.0")
-
+    implementation("com.eclipsesource.j2v8:j2v8_${OsUse({ "windows" }, { "linux" }, { "mac" })}_x86_64:4.8.0")
+    
     // http
     implementation("io.vertx:vertx-core:3.9.4")
     implementation("io.vertx:vertx-web:3.9.4")
