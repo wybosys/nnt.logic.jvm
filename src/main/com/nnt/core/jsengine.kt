23ca7dd5e9f774120fe39d1eb641444584f45ac7
wybosys <wybosys@gmail.com>
2020-11-18 11:13:54 +0800

    apidoc

--- src/main/com/nnt/core/jsengine.kt
+++ src/main/com/nnt/core/jsengine.kt
@@ -5,6 +5,7 @@ import com.eclipsesource.v8.*
 private class V8T {
     companion object {
 
+        // 从v8类型转换为kt类型
         fun array(arr: V8Array): List<Any?> {
             val r = mutableListOf<Any?>()
             for (i in 0 until arr.length()) {
@@ -13,6 +14,7 @@ private class V8T {
             return r
         }
 
+        // 从kt类型转换为v8类型
         fun array(v8: V8, vararg p: Any?): V8Array {
             val r = V8Array(v8)
             p.forEach {
@@ -20,6 +22,8 @@ private class V8T {
                     r.pushNull()
                 } else if (it is Array<*>) {
                     r.push(array(v8, *it))
+                } else if (it is List<*>) {
+                    r.push(array(v8, *it.toTypedArray()))
                 } else if (it is Map<*, *>) {
                     r.push(map(v8, it))
                 } else if (it is JsCallback) {
@@ -34,6 +38,7 @@ private class V8T {
             return r
         }
 
+        // 从kt类型转换为v8类型
         fun map(v8: V8, ps: Map<*, *>): V8Object {
             val r = V8Object(v8)
             ps.forEach { (k, v) ->
@@ -48,6 +53,8 @@ private class V8T {
                     r.add(ks, v)
                 } else if (v is Array<*>) {
                     r.add(ks, array(v8, *v))
+                } else if (v is List<*>) {
+                    r.add(ks, array(v8, *v.toTypedArray()))
                 } else if (v is Map<*, *>) {
                     r.add(ks, map(v8, v))
                 } else if (v is JsCallback) {
@@ -60,6 +67,7 @@ private class V8T {
             return r
         }
 
+        // 从kt类型转换为v8类型
         fun callback(v8: V8, cb: (receiver: V8Object, parameters: V8Array) -> Unit): V8Function {
             return V8Function(v8, object : JavaCallback {
                 override fun invoke(receiver: V8Object, parameters: V8Array): Any {
